9663ab4e1a32224f7c0978e5e6563e807c47b044
Will Wright pc <wawwright@gmail.com>
2016-04-04 21:25:59 -0600

    Added Ring.js

---
+++ Ring.js
@@ -0,0 +1,18 @@
+var uiParams = [
+  {
+       name: "size",
+       desc: "Size",
+       type: "double",
+       rangeMin: 5,
+       rangeMax: 10,
+       step: 0.5,
+       defaultVal: 6
+  }
+];
+
+function main(args) {
+  var s = args.size * MM;
+  var box = new Box(s,s,s);
+  
+  return new Scene(box, new Bounds(-s,s,-s,s,-s,s));
+}
